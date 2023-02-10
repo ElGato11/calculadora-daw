4c9a45dcf5a23bbcac0222d31560508636cd678e
usuario <usuario@Alex>
2023-02-10 11:11:56 +0100

    calculadora inicial

---
+++ calculadora-daw/src/calculadora/daw/Calculadora.java
@@ -0,0 +1,36 @@
+package calculadora.daw;
+
+/**
+ *
+ * @author usuario
+ */
+public class Calculadora {
+       private int op1;
+       private int op2;
+       private int resultado;
+    
+      public int suma(int op1, int op2){
+          this.op1=op1;
+          this.op2=op2;
+          this.resultado=op1+op2;
+         return this.resultado;
+      }
+      public int resta(int op1, int op2){
+          this.op1=op1;
+          this.op2=op2;
+         this.resultado=op1-op2;
+         return this.resultado;
+      }
+      public int multiplica(int op1, int op2){
+          this.op1=op1;
+          this.op2=op2;
+         this.resultado=op1*op2;
+         return this.resultado;
+      }
+      public int divide(int op1, int op2){
+          this.op1=op1;
+          this.op2=op2;
+         this.resultado=op1/op2;
+         return this.resultado;
+      }
+}
